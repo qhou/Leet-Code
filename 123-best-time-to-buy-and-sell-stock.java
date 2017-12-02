0b7a6f97f38d1261205ba908099502f3fda1d44c
QuinnHou <queiyun.hou@gmail.com>
2017-12-02 12:35:22 -0800

    Completed 123 hard on 12/2

---
+++ 123-best-time-to-buy-and-sell-stock.java
@@ -0,0 +1,27 @@
+/*
+Say you have an array for which the ith element is the price of a given stock on day i.
+
+Design an algorithm to find the maximum profit. You may complete at most two transactions.
+
+Note:
+You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+
+Completed with hints
+
+*/
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        //use iterative dp 
+        int buy1 = Integer.MIN_VALUE, sell1 = 0, buy2 = Integer.MIN_VALUE, sell2 = 0;
+        //set sell1, sell2 = 0, since we only want to sell if there is profit
+        //set buy1, buy2 = -infinity, since buying will always be negative
+        for (int i = 0; i < prices.length; i++){
+            buy1 = Math.max(buy1, -prices[i]);
+            sell1 = Math.max(sell1, buy1 + prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
+        }
+        return sell2;
+    }
+}
